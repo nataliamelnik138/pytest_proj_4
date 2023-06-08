1c06401f414b5d054bd8932828298efb49f198dc
Natalia Melnik <nataliamelnik138@gmail.com>
2023-06-08 23:20:05 +0600

    Fixed functions using fixtures

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,16 +1,39 @@
-from utils import arrs
 import pytest
 
-
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
-    assert arrs.get([1, 2, 3], -1, "test") == "test"
+from utils.arrs import get
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([], 0) == []
-    assert arrs.my_slice([1, 2, 3], -4, 2) == [1, 2]
-    assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
+@pytest.mark.parametrize('array, index, expected, default', [
+    ([1, 2, 3], 1, 2, "test"),
+    ([], 0, "test", "test"),
+    ([1, 2, 3], -1, "test", "test")
+])
+
+def test_get(array, index, expected, default):
+    assert get(array, index, default) == expected
+# Функция без фикстур
+# def test_get():
+#     assert arrs.get([1, 2, 3], 1, "test") == 2
+#     assert arrs.get([], 0, "test") == "test"
+#     assert arrs.get([1, 2, 3], -1, "test") == "test"
+
+
+@pytest.mark.parametrize('array, start, end, expected', [
+    ([1, 2, 3, 4], 1, 3, [2, 3]),
+    ([1, 2, 3], 1, 3, [2, 3]),
+    ([], 0, 0,  []),
+    ([1, 2, 3], -4, 2, [1, 2]),
+    ([1, 2, 3], -2, [2, 3])
+])
+
+def est_slice(array, start, end, expected):
+    assert get(array, start, end) == expected
+
+
+# Функция без фикстур
+# def test_slice():
+#     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+#     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+#     assert arrs.my_slice([], 0) == []
+#     assert arrs.my_slice([1, 2, 3], -4, 2) == [1, 2]
+#     assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
